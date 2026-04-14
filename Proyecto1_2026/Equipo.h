da2d7fdc92ed31eff22fcf3026eedd5ef72b3f12
Diego <diegomenahs04@gmail.com>
2026-04-14 15:51:15 -0600

    asjhasflasfnasDSF

--- Proyecto1_2026/Equipo.h
+++ Proyecto1_2026/Equipo.h
@@ -3,7 +3,29 @@
 using namespace std;
 class Equipo{
 private:
-	string id
-
+	string id;
+	string nombre;
+	int criticidad;
+	bool estado;
+	int tiempoActivo;
+	double prioridad;
+public:
+	Equipo(string id, string nombre, int criticidad, bool estado, int tiempoActivo, double prioridad);
+	~Equipo();
+	string getID();
+	String getNombre();
+	int getCriticidad();
+	bool getestado();
+	int getTiempoActivo();
+	double getPrioridad();
+	//stes
+	void setID();
+	void setNombre();
+	void setCriticidad();
+	void setEstado();
+	void setTiempoActivo();
+	void setPrioridad();
+	//To String
+	string MostrarEquipo();
 };
 
